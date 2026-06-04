9180512a738697d4629c4079436409fb62ecba79
jspmern <jspmern@gmail.com>
2026-06-04 23:43:33 +0530

    scuessfully tested mcp server and mcp client

--- src/index.ts
+++ src/index.ts
@@ -69,3 +69,26 @@ class MCPClient {
 }
   
 }
+
+
+async function main() {
+  if (process.argv.length < 3) {
+    console.log("Usage: node index.ts <path_to_server_script>");
+    return;
+  }
+  /**Here create the instance for server */
+  const mcpClient = new MCPClient();
+  try {
+    await mcpClient.connectToServer(process.argv[2]);
+    // await mcpClient.chatLoop();
+  } catch (e) {
+    console.error("Error:", e);
+    // await mcpClient.cleanup();
+    // process.exit(1);
+  } finally {
+    // await mcpClient.cleanup();
+    // process.exit(0);
+  }
+}
+
+main();
